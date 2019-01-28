57c3994e46d25925a2746a0da1bbca01db2477d3
Shelley Suhling <shelleysuhling@Shelleys-MacBook-Pro.local>
2019-01-28 11:15:07 -0800

    added event filtering function to show only future events

--- src/containers/Events.js
+++ src/containers/Events.js
@@ -7,25 +7,61 @@ import * as eventsActions from '../actions/eventsActions';
 import PropTypes from 'prop-types';
 import EventBlock from '../components/EventBlock'
 import { Card, Icon } from 'semantic-ui-react'
+import moment from 'moment'
 
 import * as _ from 'lodash'
 
 class Events extends Component {
     componentWillMount() {
         this.props.eventsActions.requestEvents()
+        this.filterUpcomingEvents()
     }
 
-    renderEvents() {
+    filterUpcomingEvents() {
         let { events } = this.props
-        return _.map(events.eventsList, (e => { return <EventBlock key={e.title} event={e} /> }))
+        let returned_events = _.forEach(events, (e) => {
+            console.log(e.startDate < new Date(), e)
+            return e.startDate < new Date()
+        })
+
+        console.log('returned_events', returned_events)
+
+    }
+
+    toDate = (date) => {
+        //for whatever reason, firebase's .toDate() method of a timestamp wasn't working so I made my own
+        return moment(date.seconds * 1000)
+    }
+
+    collectEventsByView = (view, events) => {
+
+        if (view === "future") {
+            return _.filter(events.eventsList, (e => {
+                return this.toDate(e.startTime).isAfter(new moment())
+            }))
+        } else if (view === "past") {
+            return _.filter(events.eventsList, (e => {
+                return this.toDate(e.startTime).isBefore(new moment())
+            }))
+        }
+    }
+
+    renderEvents(view_events) {
+        return _.map(view_events, (e => {
+            console.log(e)
+            return <EventBlock key={e.title} event={e} />
+        }))
     }
 
     render() {
+
+        let view_events = this.collectEventsByView('future', this.props.events)
+        console.log(view_events)
         return (
             <div className="content-container">
                 <div className="events-container">
                     <Card.Group>
-                        {this.renderEvents()}
+                        {this.renderEvents(view_events)}
                     </Card.Group>
                 </div>
             </div >
